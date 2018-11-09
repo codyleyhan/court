efbc38611df5e4d7aa5832af59977bf23e7c580e
Cody Ley-Han <codyleyhan@gmail.com>
2018-11-09 15:02:26 -0800

    change from string to int for user id

--- backend/court/users/models.py
+++ backend/court/users/models.py
@@ -6,7 +6,7 @@ from court.chats.models import thread_users
 class User(db.Model):
   __tablename__ = 'users'
 
-  id = db.Column(db.String(128), primary_key=True)
+  id = db.Column(db.BigInteger, primary_key=True)
   email = db.Column(db.String(128), unique=True, nullable=False)
   profile = db.relationship('Profile', backref='user', lazy=True, uselist=False)
   threads = db.relationship('Thread', secondary=thread_users,
