42eb419389cbab74895375892fb061a4061051d8
Cody Ley-Han <codyleyhan@gmail.com>
2018-11-09 14:49:16 -0800

    update for postgres in prod

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,15 @@
+import os
+
 from flask_socketio import SocketIO
 
 from court.app import create_app
+from court.config import ProductionConfig, DevelopmentConfig
 
-app = create_app()
+env = os.getenv('ENV')
+config = ProductionConfig if env == 'production' else DevelopmentConfig
+
+
+app = create_app(config)
 
 if __name__ == '__main__':
   socketio = SocketIO(app)
