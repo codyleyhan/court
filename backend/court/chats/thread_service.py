fbc71f780e3c25e505759b0a98e6b5ea30a43dfb
Cody Ley-Han <codyleyhan@gmail.com>
2018-11-09 10:00:03 -0800

    Add more documentation

--- backend/court/chats/thread_service.py
+++ backend/court/chats/thread_service.py
@@ -4,12 +4,33 @@ from court.database import db
 from court.errors import AuthorizationError, NotFoundError
 
 class ThreadService:
+  """
+  Handles all business logic for creating and managing user's chat threads.
+  """
+
+
   def __init__(self, db_conn=db, message_store=Message, thread_store=Thread):
+    """
+    Constructs a new ThreadService.
+
+    :param db_conn: a SQLAlchemy database connection
+    :param message_store: ORM object to create/query messages
+    :param thread_store: ORM object to create/query chat threads
+    """
+
     self.message_store = message_store
     self.thread_store = thread_store
     self.db = db_conn
 
+
   def create_thread(self, user_1, user_2):
+    """
+    Creates and persists a new chat thread between the users passed.
+
+    :param user_1: is a User object
+    :param user_2: is a User object
+    :return: returns a Thread object with the two users associated
+    """
     if user_1 is None or user_2 is None:
       raise RuntimeError()
 
@@ -24,6 +45,14 @@ class ThreadService:
     return thread
 
   def get_thread(self, current_user_id, thread_id):
+    """
+    Queries for a thread with the passed id.  Will also check authorization of 
+    user.
+
+    :param current_user_id: the id the user requesting the thread information
+    :param thread_id: the id of the thread being requested
+    :return: a Thread object associated with the thread_id
+    """
     thread = self.thread_store.query.get(thread_id)
     if thread is None:
       raise NotFoundError("No thread found with id of %r" % thread_id)
@@ -31,13 +60,31 @@ class ThreadService:
       raise AuthorizationError()
     return thread
 
-  def user_is_in_thread(self, current_user_id, thread):
+  def user_is_in_thread(self, user_id, thread):
+    """
+    Checks if a user is authorized to be in a thread.
+
+    :param user_id: the user id being checked
+    :param thread: Thread object that is being checked
+    :return: true if the user is authorized to see the thread
+    """
     for user in thread.users:
-      if current_user_id == user.id:
+      if user_id == user.id:
         return True
     return False
 
   def get_messages(self, current_user_id, thread_id, first=50, after_id=-1, before_id=-1):
+    """
+    Fetches paginated thread messages in descending order.  If both after_id and
+    before_id are passed then only after_id will be used.
+
+    :param current_user_id: the user requesting the messages
+    :param thread_id: the id of the thread the messages are being requested
+    :param first: the number of messages to return upto
+    :param after_id: if passed returns all messages with id greater than after_id
+    :param before_id: if passed returns all messages with id less than before_id
+    :return: list of thread messages
+    """
     thread = self.get_thread(current_user_id, thread_id)
 
     if after_id != -1 and before_id != -1:
@@ -55,6 +102,12 @@ class ThreadService:
     return messages
   
   def add_message(self, message):
+    """
+    Adds a message to a thread.
+
+    :param message: a message object
+    :return: message with id added
+    """
     if message is None:
       raise RuntimeError()
 
