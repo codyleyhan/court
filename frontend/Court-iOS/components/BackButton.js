8ae72a03cb002e2481aa8195627f98b42c0a3abf
RivMist <rivmist@gmail.com>
2018-11-14 01:30:01 -0800

    Add BackButton display snapshot, update PropType to enforce a StackNavigator

--- frontend/Court-iOS/components/BackButton.js
+++ frontend/Court-iOS/components/BackButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'expo';
+import { StackNavigator } from 'react-navigation';
 
 import { TouchableOpacity, View } from 'react-native';
 
--- frontend/Court-iOS/components/__tests__/BackButton-test.js
+++ frontend/Court-iOS/components/__tests__/BackButton-test.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Avatar from '../BackButton';
+import BackButton from '../BackButton';
 import renderer from 'react-test-renderer';
 
 it('Renders BackButton with navigation', () => {
