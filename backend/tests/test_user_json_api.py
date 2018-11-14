47892dccf10e5a769f0af0515d69f863bb0a38e5
Cody Ley-Han <codyleyhan@gmail.com>
2018-11-14 13:02:37 -0800

    add check to make sure user is in db

--- backend/tests/test_user_json_api.py
+++ backend/tests/test_user_json_api.py
@@ -2,6 +2,8 @@ import json
 
 import pytest
 
+from court.users.models import User
+
 def test_login_new_user(app, requests_mock):
   facebook_token = 'mocksodoesntmatter'
   facebook_url = 'https://graph.facebook.com/me?fields=id,first_name,last_name,email,picture.height(300).width(300)&access_token=' + facebook_token
@@ -24,6 +26,11 @@ def test_login_new_user(app, requests_mock):
   court_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAyNzczNDM3NDAwMjUxLCJpc19hZG1pbiI6ZmFsc2V9.qVJ99o4cG1xsHAac2ztrBsyExST76pDlzhnJx9Nxt0s'
   requests_mock.get(facebook_url, json=mocked_fb_resp)
 
+  with app.app_context():
+    # ensure the user is not in the db
+    user = User.query.filter(User.id == '102773437400251').one_or_none()
+    assert user is None
+
   with app.test_client() as client:
     params = { 'access_token': facebook_token }
     resp = client.post('/api/users', query_string=params)
@@ -32,3 +39,8 @@ def test_login_new_user(app, requests_mock):
     assert data['token'] == court_jwt
     assert data['user']['email'] == 'kfgzlneeuo_1541453454@fbnw.net'
     assert data['user']['id'] == '102773437400251'
+
+    with app.app_context():
+      # user should now be in db
+      user = User.query.filter(User.id == '102773437400251').one_or_none()
+      assert user is not None
