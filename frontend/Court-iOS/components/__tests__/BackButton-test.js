9af1936ea4abd8e0146f478a7656df39ae865281
RivMist <rivmist@gmail.com>
2018-11-14 01:38:27 -0800

    Add GenderSelector snapshots, update BackButton tests

--- frontend/Court-iOS/components/__tests__/BackButton-test.js
+++ frontend/Court-iOS/components/__tests__/BackButton-test.js
@@ -6,6 +6,10 @@ import renderer from 'react-test-renderer';
 it('Renders BackButton with navigation', () => {
   const navigation = { navigate: jest.fn() };
 
+  // These are expected, and can be used later
+  consoleSpy = jest.spyOn(console, "error").mockImplementation();
+  consoleSpyWarn = jest.spyOn(console, "warn").mockImplementation();
+
   const tree = renderer.create(<BackButton navigation={navigation}/>).toJSON();
 
   expect(tree).toMatchSnapshot();
