e5c0c5a46b035d6b6f3cb9de1b37c6d9587e9e35
RivMist <rivmist@gmail.com>
2018-11-14 14:20:01 -0800

    Add TabBarIcon tests

---
+++ frontend/Court-iOS/components/__tests__/TabBarIcon-test.js
@@ -0,0 +1,16 @@
+import 'react-native';
+import React from 'react';
+import TabBarIcon from '../TabBarIcon';
+import renderer from 'react-test-renderer';
+
+it('Renders Default TabBarIcon', () => {
+  const onPress = jest.fn();
+  const tree = renderer.create(
+    <TabBarIcon
+      name="Inbox"
+      focused={true}
+    />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
