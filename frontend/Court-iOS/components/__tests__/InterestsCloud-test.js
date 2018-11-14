469eb02ff515936cad0755cf3b9178d810a0bb76
RivMist <rivmist@gmail.com>
2018-11-14 14:10:59 -0800

    Update InterestsCloud tests, add snapshots for testing

--- frontend/Court-iOS/components/__tests__/InterestsCloud-test.js
+++ frontend/Court-iOS/components/__tests__/InterestsCloud-test.js
@@ -6,7 +6,7 @@ import renderer from 'react-test-renderer';
 it('Renders Default InterestsCloud', () => {
   const mocks = { onAddInterest: jest.fn(), onRemoveInterest: jest.fn() };
   const interests = {'kgs://12hv3': {title: 'The Office', description: 'American Sitcom', imgUrl: 'test.url'}};
-  const recommendations = [{'kgs://jsh12': {title: 'Interest Rec 1'}}, {'kgs://3jh43': {title: 'Interest Rec 1'}}];
+  const recommendations = [{id: 'kgs://jsh12', title: 'Interest Rec 1'}, {id: 'kgs://3jh43', title: 'Interest Rec 2'}];
   const tree = renderer.create(
     <InterestsCloud
       interests={interests}
