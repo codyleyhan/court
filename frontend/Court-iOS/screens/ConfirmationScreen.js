ce6da3139f7e27a152cf2554157e8344fdfd6208
RivMist <rivmist@gmail.com>
2018-11-14 22:45:42 -0800

    Add Basic Confirmation Screen

--- frontend/Court-iOS/screens/ConfirmationScreen.js
+++ frontend/Court-iOS/screens/ConfirmationScreen.js
@@ -17,44 +17,21 @@ import Colors from '../constants/Colors';
 
 import LoginButton from '../components/LoginButton';
 import BackButton from '../components/BackButton';
-import InterestsFinder from '../components/InterestsFinder';
-import InterestsItem from '../components/InterestsItem';
 
-export default class InterestsScreen extends React.Component {
+export default class ConfirmationScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
-  constructor(props) {
-    super(props);
-    this.removeInterest = this.removeInterest.bind(this);
-    this.state = { interests: {} };
-  }
-
-  addInterest(id, interest) {
-    let interests = this.state.interests;
-    interests[id] = interest;
-    this.setState({ interests });
-  }
-
-  removeInterest = (id) => {
-    let interests = this.state.interests;
-    delete interests[id];
-    this.setState({ interests });
-  }
-
-  goNext(user) {
-    // Publish interests to API, navigate to next page
-    // TODO(rivmist): update the backend with these changes
-    this.props.navigation.navigate('Confirmation', {user: user});
+  goNext() {
+    this.props.navigation.navigate('Auth');
   }
 
   render() {
-    const { interests } = this.state;
     const user = this.props.navigation.getParam('user', null);
     const user_name = user.first_name;
     const profile_url = user.picture.data.url;
-    const remove = this.removeInterest.bind(this);
+
     return (
       <View style={styles.container}>
         <BackButton navigation={this.props.navigation} />
@@ -68,49 +45,21 @@ export default class InterestsScreen extends React.Component {
             </Mask>
           </View>
         </Transition>
-        {Object.keys(interests).length > 0 ? (
-          <View style={styles.interestsListWrapper}>
-            <ScrollView
-              horizontal={true}
-              style={styles.interestsList}
-            >
-              // Put selected interests here
-              {Object.keys(interests).map(function(key) {
-                return (
-                  <View key={key}>
-                    <InterestsItem
-                      id={key}
-                      disableSelect={true}
-                      title={interests[key].title}
-                      onRemoveInterest={remove}
-                      selected={true}
-                      showRemoveIcon={true}
-                    />
-                  </View>
-                );
-              })}
-            </ScrollView>
-          </View>
-        ) : (
-          <View style={{alignItems: 'center', justifyContent: 'center'}}>
-            <Transition appear="horizontal">
-              <Text style={styles.welcomeText}>{"Set up your interests"}</Text>
-            </Transition>
-            <Transition appear="horizontal">
-              <Text style={styles.subText}>{'These help us to find you matches'}</Text>
-            </Transition>
-          </View>
-        )}
-        <InterestsFinder interests={interests} onAddInterest={this.addInterest.bind(this)} onRemoveInterest={this.removeInterest.bind(this)} />
-        {Object.keys(interests).length > 0 && (
-          <View style={styles.continueButton}>
-            <TouchableOpacity onPress={() => this.goNext(user)} activeOpacity={0.75}>
-              <Text style={styles.continueText}>
-                Continue
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <Transition appear="horizontal">
+            <Text style={styles.welcomeText}>{`Congrats ${user_name}!`}</Text>
+          </Transition>
+          <Transition appear="horizontal">
+            <Text style={styles.subText}>{'Your account is all set up'}</Text>
+          </Transition>
+        </View>
+        <View style={styles.continueButton}>
+          <TouchableOpacity onPress={() => this.goNext(user)} activeOpacity={0.75}>
+            <Text style={styles.continueText}>
+              Continue
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
