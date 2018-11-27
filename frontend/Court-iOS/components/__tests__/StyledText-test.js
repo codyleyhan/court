48ccf605704ccbea46f5c454f17473a3a91c89cd
RivMist <rivmist@gmail.com>
2018-11-26 23:57:00 -0800

    Update Snapshot tests for updated signup flow

--- frontend/Court-iOS/components/__tests__/StyledText-test.js
+++ frontend/Court-iOS/components/__tests__/StyledText-test.js
@@ -1,11 +1,11 @@
 import 'react-native';
 import React from 'react';
-import { MonoText } from '../StyledText';
+import { RegularText } from '../StyledText';
 import renderer from 'react-test-renderer';
 
 it('Renders Styled text', () => {
   // TODO(rivmist): change this to our custom text once loaded
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  const tree = renderer.create(<RegularText>Snapshot test!</RegularText>).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
