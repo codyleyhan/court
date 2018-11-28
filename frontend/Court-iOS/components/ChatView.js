5bd4afcb45d5e343d9e69cd86add3d99b03f6097
RivMist <rivmist@gmail.com>
2018-11-27 16:28:52 -0800

    Chat styling

--- frontend/Court-iOS/components/ChatView.js
+++ frontend/Court-iOS/components/ChatView.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'expo';
-import { GiftedChat, Send } from 'react-native-gifted-chat'
+import { Bubble, Composer, InputToolbar, GiftedChat, MessageText, Send, Time } from 'react-native-gifted-chat'
 
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Mask from 'react-native-mask';
 import Colors from '../constants/Colors';
-
 
 /**
 * Creates header with centered text, and optional left and right components
@@ -39,11 +38,59 @@ export default class ChatView extends React.Component {
     }))
   }
 
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: styles.leftBubble,
+          right: styles.rightBubble,
+        }}
+      />
+    );
+  }
+
+  renderMessageText(props) {
+    return (
+      <MessageText
+        {...props}
+        textStyle={{
+          left: styles.leftText,
+          right: styles.rightText,
+        }}
+      />
+    );
+  }
+
+  renderTime(props) {
+    return (
+      <Time
+        {...props}
+        containerStyle={{ left: [styles.timeContainer], right: [styles.timeContainer] }}
+        textStyle={{
+          left: styles.leftText,
+          right: styles.rightText,
+        }}
+      />
+    );
+  }
+
+  renderComposer(props) {
+    return (
+      <Composer
+        {...props}
+        textInputStyle={styles.composer}
+        placeholder={'Type message...'}
+        multiline={true}
+      />
+    );
+  }
+
   renderSend(props) {
     const sendButton = (
       <View style={{marginRight: 10, marginBottom: 5}}>
         <Mask shape={'circle'}>
-          <View style={{width: 35, height: 35, backgroundColor: Colors.peach, alignItems: 'center', paddingRight: 2, paddingTop: 1}}>
+          <View style={styles.sendButton}>
             <Icon.Ionicons
               name='ios-send'
               size={35}
@@ -65,7 +112,13 @@ export default class ChatView extends React.Component {
       <GiftedChat
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
+        renderBubble={this.renderBubble}
         renderSend={this.renderSend}
+        renderTime={this.renderTime}
+        renderMessageText={this.renderMessageText}
+        renderComposer={this.renderComposer}
+        minInputToolbarHeight={40}
+        bottomOffset={76}
         user={{
           _id: 1,
         }}
@@ -75,24 +128,90 @@ export default class ChatView extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  headerContainer: {
-    flexDirection: 'row',
-    paddingHorizontal: 20,
-    paddingTop: 40,
-    paddingBottom: 10,
-    justifyContent: 'center',
+  sendButton: {
+    width: 35,
+    height: 35,
+    backgroundColor: Colors.peach,
     alignItems: 'center',
-    borderBottomColor: '#21ACA5',
-    borderBottomWidth: 2,
-    backgroundColor: '#fff',
+    paddingRight: 2,
+    paddingTop: 2,
   },
-  headerTitle: {
-    color: '#21ACA5',
+  leftBubble: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: 4 },
+        shadowOpacity: 0.12,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 15,
+      },
+    }),
+    backgroundColor: Colors.coral,
+    marginBottom: 4,
+  },
+  rightBubble: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: 4 },
+        shadowOpacity: 0.12,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 15,
+      },
+    }),
+    backgroundColor: 'white',
+    marginBottom: 4,
+  },
+  leftText: {
     fontFamily: 'orkney-medium',
-    paddingTop: 10,
-    fontSize: 30,
-    flex: 1,
-    textAlign: 'center',
+    color: 'white',
+    paddingTop: 3,
+    marginBottom: -2,
+  },
+  rightText: {
+    fontFamily: 'orkney-medium',
+    color: 'black',
+    paddingTop: 3,
+    marginBottom: -2,
+  },
+  leftTime: {
+    fontFamily: 'orkney-light',
+    textAlign: 'left',
+    color: 'white',
+  },
+  rightTime: {
+    fontFamily: 'orkney-light',
+    textAlign: 'right',
+    color: 'black',
+  },
+  composer: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: 4 },
+        shadowOpacity: 0.12,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 15,
+      },
+    }),
+    backgroundColor: 'white',
+    marginBottom: 4,
+    fontFamily: 'orkney-medium',
+    fontSize: 20,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    paddingTop: 8,
+    marginRight: 10,
+    lineHeight: 25,
+  },
+  inputToolbar: {
+    backgroundColor: 'green',
   }
 });
 
